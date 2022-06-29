a3f67e275c8a39b68b0c319d7f0c745c3f1531f8
ZFeng1005 <85599067+ZFeng1005@users.noreply.github.com>
2022-06-29 15:37:14 +0800

    Del
    删除大部分过期脚本

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -13,12 +13,8 @@
 10 0 * * * node /scripts/jd_jintie.js >> /scripts/logs/jd_jintie.log 2>&1
 #jd_daily_lottery
 13 1,22,23 * * * node /scripts/jd_daily_lottery.js >> /scripts/logs/jd_daily_lottery.log 2>&1
-#金榜创造营
-40 9,21 * * * node /scripts/jd_gold_creator.js >> /scripts/logs/jd_gold_creator.log 2>&1
 #京喜牧场
 20 * * * * node /scripts/jd_jxmc.js >> /scripts/logs/jd_jxmc.log 2>&1
-#东东电竞经理
-47 10 * * * node /scripts/jd_djjl.js >> /scripts/logs/jd_djjl.log 2>&1
 #众筹许愿池
 20 12 * * * node /scripts/jd_wishingPool.js >> /scripts/logs/jd_wishingPool.log 2>&1
 #图形验证
@@ -26,20 +22,12 @@
 #京东金融摇钱树助力
 # MMdou
 21 9 * * * node /scripts/jd_MMdou.js >> /scripts/logs/jd_MMdou.log 2>&1
-#女装盲盒
-54 9 * * * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
 # 领卷中心签到
 15 10 * * * node /scripts/jd_ccSign.js >> /scripts/logs/jd_ccSign.log 2>&1
-# 内容鉴赏官
-15 2,5 * * * node /scripts/jd_connoisseur.js >> /scripts/logs/jd_connoisseur.log 2>&1
 # 京喜财富岛合成月饼
 5 * * * * node /scripts/jd_cfd_mooncake.js >> /scripts/logs/jd_cfd_mooncake.log 2>&1
 # 送豆得豆
 24 0,12 * * * node /scripts/jd_sendBeans.js >> /scripts/logs/jd_sendBeans.log 2>&1
-#东东世界
-15 3,16 * * * node /scripts/jd_ddworld.js >> /scripts/logs/jd_ddworld.log 2>&1
-#jd_ddworld_exchange
-0 0,9 * * * node /scripts/jd_ddworld_exchange.js >> /scripts/logs/jd_ddworld_exchange.log 2>&1
 # 京东小魔方
 10 2,8 * * * node /scripts/jd_mf.js >> /scripts/logs/jd_mf.log 2>&1
 #芥么签到
@@ -58,20 +46,12 @@
 30 1,23 * * * node /scripts/jd_cscx.js >> /scripts/logs/jd_cscx.log 2>&1
 # 赚京豆(微信小程序)
 12 * * * * node /scripts/jd_syj.js >> /scripts/logs/jd_syj.log 2>&1
-# 饭粒
-20 2,18 * * * node /scripts/jd_fanli.js >> /scripts/logs/jd_fanli.log 2>&1
-#兑换7天过期喜豆
-30 20 * * * node /scripts/jd_exchangejxbeans.js >> /scripts/logs/jd_exchangejxbeans.log 2>&1
 #京东小魔方--收集兑换
 31 8 * * * node /scripts/jd_mofang_ex.js >> /scripts/logs/jd_mofang_ex.log 2>&1
 # 财富岛珍珠兑换
 59 0-23/1 * * * node /scripts/jd_cfd_pearl_ex.js >> /scripts/logs/jd_cfd_pearl_ex.log 2>&1
 # 天天压岁钱
 33 0,14,20 * * * node /scripts/jd_ttysq.js >> /scripts/logs/jd_ttysq.log 2>&1
-#jd_xfxd
-18 0 * * * node /scripts/jd_xfxd.js >> /scripts/logs/jd_xfxd.log 2>&1
-# 京喜领88元红包
-30 1,6,12,21 * * * node /scripts/jd_jxlhb.js >> /scripts/logs/jd_jxlhb.log 2>&1
 #jd_15_5
 58 59 6,9,14,17,20 * * * node /scripts/jd_15_5.js >> /scripts/logs/jd_15_5.log 2>&1
 #jd_29_8
@@ -80,46 +60,8 @@
 58 59 6,9,14,17,20 * * * node /scripts/jd_5_2.js >> /scripts/logs/jd_5_2.log 2>&1
 #jd_10_4
 58 59 7,10,13,15,19 * * * node /scripts/jd_10_4.js >> /scripts/logs/jd_10_4.log 2>&1
-#店铺签到
-15 2,14 * * * node /scripts/jd_dpqd.js >> /scripts/logs/jd_dpqd.log 2>&1
-# 通天塔签到共建
-0 0,3 * * * node /scripts/jd_m_sign.js >> /scripts/logs/jd_m_sign.log 2>&1
-#宠汪汪乐园
-20 0-23/3 * * * node /scripts/jd_joy_park.js >> /scripts/logs/jd_joy_park.log 2>&1
-#汪汪乐园提现
-15 15 * * * node /scripts/jd_joy_withdraw.js >> /scripts/logs/jd_joy_withdraw.log 2>&1
-#天天拼图
-20 0,16 * * * node /scripts/jd_ttpt.js >> /scripts/logs/jd_ttpt.log 2>&1
-#微信签到领红包
-3 0,11 * * * node /scripts/jd_wq_wxsign.js >> /scripts/logs/jd_wq_wxsign.log 2>&1
 #jd_pay_contract
 35 21 */3 * * node /scripts/jd_pay_contract.js >> /scripts/logs/jd_pay_contract.log 2>&1
-#省钱大赢家
-20 0-23/2 * * * node /scripts/jd_flipcards.js >> /scripts/logs/jd_flipcards.log 2>&1
-#jd_opencard153
-30 9 17-31/2,1-16/2 5,6 * node /scripts/jd_opencard153.js >> /scripts/logs/jd_opencard153.log 2>&1
-#jd_opencard159
-30 1 24-31/2,2-19/2 5,6 * node /scripts/jd_opencard159.js >> /scripts/logs/jd_opencard159.log 2>&1
-#jd_opencard63
-30 0,9,20 1-20 6 * node /scripts/jd_opencard163.js >> /scripts/logs/jd_opencard163.log 2>&1
-#jd_opencard166
-30 0,9,20 1-20 6 * node /scripts/jd_opencard166.js >> /scripts/logs/jd_opencard166.log 2>&1
-#jd_opencard167
-10 1,10 1-20 6 * node /scripts/jd_opencard167.js >> /scripts/logs/jd_opencard167.log 2>&1
-#jd_opencard168
-20 2,11 1-20 6 * node /scripts/jd_opencard168.js >> /scripts/logs/jd_opencard168.log 2>&1
-#jd_opencard169
-40 3,12 1-20 6 * node /scripts/jd_opencard169.js >> /scripts/logs/jd_opencard169.log 2>&1
-#jd_opencard170
-30 4,13 1-20 6 * node /scripts/jd_opencard170.js >> /scripts/logs/jd_opencard170.log 2>&1
-#jd_opencard177
-25 5,14 1-20 6 * node /scripts/jd_opencard177.js >> /scripts/logs/jd_opencard177.log 2>&1
-#jd_opencard178
-50 6,15 1-20 6 * node /scripts/jd_opencard178.js >> /scripts/logs/jd_opencard178.log 2>&1
-#jd_opencard183
-33 7,16 1-20 6 * node /scripts/jd_opencard183.js >> /scripts/logs/jd_opencard183.log 2>&1
-#jd_opencard182
-28 8,17 1-20 6 * node /scripts/jd_opencard182.js >> /scripts/logs/jd_opencard182.log 2>&1
 ##############长期活动##############
 # 签到
 0 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
@@ -139,8 +81,6 @@
 1 1 * * * node /scripts/jd_redPacket.js >> /scripts/logs/jd_redPacket.log 2>&1
 # 进店领豆
 10 0 * * * node /scripts/jd_shop.js >> /scripts/logs/jd_shop.log 2>&1
-# 京东天天加速
-8 */3 * * * node /scripts/jd_speed.js >> /scripts/logs/jd_speed.log 2>&1
 # 东东超市
 11 */6 * * * node /scripts/jd_superMarket.js >> /scripts/logs/jd_superMarket.log 2>&1
 # 取关京东店铺商品
@@ -161,10 +101,6 @@
 36 0-23/4 * * * node /scripts/jd_syj.js >> /scripts/logs/jd_syj.log 2>&1
 # 京东快递签到
 23 1 * * * node /scripts/jd_kd.js >> /scripts/logs/jd_kd.log 2>&1
-# 领京豆额外奖励(每日可获得3京豆)
-33 0-23/4 * * * node /scripts/jd_bean_home.js >> /scripts/logs/jd_bean_home.log 2>&1
-# 京东直播(每日18豆)
-10 12-14 * * * node /scripts/jd_live.js >> /scripts/logs/jd_live.log 2>&1
 # 微信小程序京东赚赚
 30 0,1 * * * node /scripts/jd_jdzz.js >> /scripts/logs/jd_jdzz.log 2>&1
 # 导到所有互助码
@@ -173,8 +109,6 @@
 27 7,15 * * * node /scripts/jd_cash.js >> /scripts/logs/jd_cash.log 2>&1
 # 闪购盲盒
 27 8 * * * node /scripts/jd_sgmh.js >> /scripts/logs/jd_sgmh.log 2>&1
-# 京东秒秒币
-10 7 * * * node /scripts/jd_ms.js >> /scripts/logs/jd_ms.log 2>&1
 # 京喜财富岛
 10 * * * *  node /scripts/jd_cfd.js >> /scripts/logs/jd_cfd.log 2>&1
 # 京东极速版
